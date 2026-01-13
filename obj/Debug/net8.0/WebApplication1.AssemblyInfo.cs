f6a109f3eea09c49257bdbd3b2f1373e55d004ca
Nicolas Rocchia <nrocchia@ext.sancorseguros.com>
2026-01-13 01:03:40 -0300

    no message

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a97ee0d608e9567f37c0b548680cf7b0e0873ca3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db4b5499a33adb62ca7ee0d21b7603ff872583a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
